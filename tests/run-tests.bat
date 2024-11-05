17ccd1af49a3f4988ae7f61a8b1c86c77b32c91d
Jose Costa Teixeira <jose.a.teixeira@gmail.com>
2024-11-05 12:27:38 +0100

    prepare for pub

--- tests/run-tests.bat
+++
@@ -1,5 +0,0 @@
-
-java -jar karate-1.5.0.jar testcases/ 
-
-REM After tests are completed, open the Karate summary report in the default browser
-start "" "%CD%\target\karate-reports\karate-summary.html"
--- tests/run-tests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker-compose up -d # Start server in background
-karate test tests/ # Run tests in the tests folder
